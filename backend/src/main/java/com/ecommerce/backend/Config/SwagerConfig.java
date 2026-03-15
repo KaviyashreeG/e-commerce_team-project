20145f95b6616a99eb75898ff86f8e822ad86fed
Kishhore S <kishhore0608@gmail.com>
2026-03-15 22:24:53 +0530

    Authentication

--- backend/src/main/java/com/ecommerce/backend/Config/SwagerConfig.java
+++
@@ -1,5 +0,0 @@
-package com.ecommerce.backend.config;
-
-public class SwagerConfig {
-    
-}
--- backend/src/main/java/com/ecommerce/backend/Entity/enums/UserRole.java
+++ backend/src/main/java/com/ecommerce/backend/Entity/enums/UserRole.java
@@ -1,8 +1,8 @@
 package com.ecommerce.backend.entity.enums;
 
-
-
 public enum UserRole {
-    ADMIN,
-    CUSTOMER
+    ROLE_SUPER_ADMIN,
+    ROLE_ADMIN,
+    ROLE_SELLER,
+    ROLE_CUSTOMER
 }
